59c68dfc3763adc537fbd7a0f359bd3eb78fe29c
MubarratHossain <tahsinezab97@gmail.com>
2024-10-31 23:38:35 +0600

    Navbar and footer created from component library

--- Books-Store/src/main.jsx
+++ Books-Store/src/main.jsx
@@ -6,12 +6,29 @@ import {
   createBrowserRouter,
   RouterProvider,
 } from "react-router-dom";
+import ErrorPage from './components/ErrorPage/ErrorPage.jsx';
+import Home from './components/Home/Home.jsx';
+import Dashboard from './components/Dashboard/Dashboard.jsx';
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <Root></Root>
-    ,
+    element: <Root></Root>,
+    errorElement:<ErrorPage></ErrorPage>,
+    children:[
+        {
+            path:'/',
+            element:<Home></Home>
+
+        },
+        {
+          path:'dashboard',
+          element:<Dashboard></Dashboard>
+
+        }
+
+
+    ]
   },
 ]);
 
